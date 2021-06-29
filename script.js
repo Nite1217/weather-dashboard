bdc8369c9b7671e9e2b7e0e6048e5854bde8624e
nikol411@hotmail.com <nikol411@hotmail.com>
2021-06-28 21:42:37 -0400

    updated html and css

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var button = document.querySelector('.button')
 var inputValue = document.querySelector ('.inputValue')
-var name = document.querySelector('.name')
+var name = document.querySelector('.city')
 var name = document.querySelector('.desc')
 var name = document.querySelector('.temp')
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function(){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=d385996841fd27ab074626cf52b4c10b')
-.then(response => response.json())
-.then(data => console.log(data))
+
+.then(function(response) {
+return response.json();
+})
+.then(data => console.log
 
 .catch(err => alert("City not found!"))
 })
